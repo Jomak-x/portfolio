6158a82f030a0cf5b311237935ba0fd8392fdeb8
Jomak-x <Jakob@laise.de>
2026-04-08 17:09:43 -0400

    add picture

--- data/profile.ts
+++ data/profile.ts
@@ -2,7 +2,7 @@ export const profile = {
     name: "Jakob Laise",
     nameintro: "Hi, I'm ",
     titles: ["Software Engineer", "Computer Science Student", "Researcher", "Traveler"],
-    image: "/home/First_page/profile.png",
+    image: "/home/First_Page/profile1.png",
     university: "University of Central Florida",
     hackathonimg: ["/home/First_page/hackathon1.png", "/home/First_page/hackathon2.png", "/home/First_page/hackathon3.png", "/home/First_page/hackathon4.png"],
     personalimg: ["/home/First_page/personal1.png", "/home/First_page/personal2.png", "/home/First_page/personal3.png", "/home/First_page/personal4.png"],
